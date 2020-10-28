24ae6de08bfe30397c940dc3cc15d6fd390e9c58
bshang <bshang@student.21-school.ru>
2020-10-28 16:23:56 +0300

    20201028_16:23

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+minikube start --driver=virtualbox --memory='3900MB' --disk-size='10000MB' ;
+
+minikube addons enable dashboard ;
+minikube addons enable metallb ;
+echo "__________________________________ addons enabled <<<\n" ;
+
+eval $(minikube docker-env)
+
+echo "__________________________________ metallb" ;
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.4/manifests/namespace.yaml ;
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.4/manifests/metallb.yaml ;
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)" ;
+kubectl apply -f ./srcs/metallb/metallb.yaml ;
+
+echo "__________________________________ NGINX"
+kubectl create -f ./srcs/nginx/nginx-service.yaml
+sleep 5
+# N_IP=$(kubectl get svc nginx-service -o=custom-columns='DATA:status.loadBalancer.ingress' | sed -n 2p | cut -d ":" -f2 | tr -d "]") ;
+TEMP=$(kubectl get svc nginx-service -o=custom-columns='DATA:status.loadBalancer.ingress' | sed -n 2p | tr -d "[maip:]")
+# echo "${N_IP}" ;
+echo "${TEMP}" ;
+# docker build -t nginx-image --build-arg IP=${N_IP} ./srcs/nginx ;
+# docker build -t nginx-image --build-arg IP=${TEMP} ./srcs/nginx ;
+docker build -t nginx-image ./srcs/nginx ;
+kubectl apply -f ./srcs/nginx/nginx-deployment.yaml ;
