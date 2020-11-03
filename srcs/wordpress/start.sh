fa6f8f752d7e1afc50d75e558587a6a11875a87e
bshang <bshang@student.21-school.ru>
2020-11-03 11:33:39 +0300

    20201103_11-33 wp: error w db connection, but config is pulled

--- srcs/wordpress/start.sh
+++ srcs/wordpress/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# php -S 0.0.0.0:5050 -t  /etc/wordpress/
-php-fpm7
+php -S 0.0.0.0:5050 -t  /etc/wordpress/
+# php-fpm7
 nginx -g "daemon off;"
 /usr/bin/supervisord -c /etc/supervisord.conf
