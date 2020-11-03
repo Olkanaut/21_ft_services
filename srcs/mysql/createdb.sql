30cba3120148d05c9b82f0ed2534b86434aae3a4
bshang <bshang@student.21-school.ru>
2020-11-03 11:55:15 +0300

    20201103_11-54 mysql pod dies after 6 secs

--- srcs/mysql/createdb.sql
+++ srcs/mysql/createdb.sql
@@ -1,4 +1,15 @@
-CREATE DATABASE wordpress;
-
-GRANT ALL PRIVILEGES ON *.* TO 'bshang'@'%' IDENTIFIED BY 'mypass' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+CREATE DATABASE wordpress;
+CREATE USER 'bshang'@'%' IDENTIFIED BY 'mypass';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'bshang'@'%';
+FLUSH PRIVILEGES;
+
+-- CREATE DATABASE wordpress;
+-- GRANT ALL PRIVILEGES ON *.* TO 'bshang'@'localhost' IDENTIFIED BY 'mypass' WITH GRANT OPTION;
+-- FLUSH PRIVILEGES;
+
+-- CREATE DATABASE wordpress;
+
+-- CREATE USER 'bshang'@'localhost' IDENTIFIED BY 'mypass';
+-- GRANT ALL PRIVILEGES ON wordpress.* TO 'admin'@'localhost';
+-- FLUSH PRIVILEGES;
