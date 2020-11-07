47c87214a60f02575e969158bd5582fabb63a8fb
Ol'ga Huraskina <bshang@vo-l7.msk.21-school.ru>
2020-11-07 13:54:24 +0300

    20201107_13-49 nginx fixed - ok

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# export MINIKUBE_HOME=/Users/bshang/goinfre/.minikube ;
-# export PATH=$MINIKUBE_HOME/bin:$PATH ;
-# export KUBECONFIG=$MINIKUBE_HOME/.kube/config ;
-# export KUBE_EDITOR="code -w" ;
-# minikube start --driver=virtualbox --disk-size=5000MB;
+export MINIKUBE_HOME=/Users/bshang/goinfre/.minikube ;
+export PATH=$MINIKUBE_HOME/bin:$PATH ;
+export KUBECONFIG=$MINIKUBE_HOME/.kube/config ;
+export KUBE_EDITOR="code -w" ;
+minikube start --driver=virtualbox --disk-size=5000MB;
 
 
 # minikube start --driver=virtualbox --memory='3900MB' --disk-size='10000MB' ;
