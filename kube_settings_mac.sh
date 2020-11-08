ea7a4c221b69e908f828fea2bf014d5bfe06ec91
Ol'ga Huraskina <bshang@vo-l7.msk.21-school.ru>
2020-11-08 17:15:00 +0300

    20201108_17-14 all is well

---
+++ kube_settings_mac.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export MINIKUBE_HOME=/Users/bshang/goinfre/.minikube ;
+export PATH=$MINIKUBE_HOME/bin:$PATH ;
+export KUBECONFIG=$MINIKUBE_HOME/.kube/config ;
+export KUBE_EDITOR="code -w" ;
